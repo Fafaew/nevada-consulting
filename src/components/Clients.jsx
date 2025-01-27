f48952c23c8a141a495ac00e777b3f689dca5180
Rafael <rafael.gomest@gmail.com>
2025-01-27 18:54:13 -0300

    header | fix styles and click links

--- src/components/Clients.jsx
+++ src/components/Clients.jsx
@@ -7,7 +7,7 @@ const Clients = ({ logos }) => {
   const { t } = useTranslation();
 
   return (
-    <section className='py-16 bg-gray-100'>
+    <section id='clients' className='py-16 bg-gray-100'>
       <div className='container mx-auto px-4'>
         <h2 className='text-3xl font-bold text-center mb-12'>
           {t('clients.title')}
