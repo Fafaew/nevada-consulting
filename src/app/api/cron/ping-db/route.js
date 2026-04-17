ebfe75a2744525a7bbc20227dd61bf2c2e8e2a8d
Rafael <rafael.gomest@gmail.com>
2026-04-17 16:25:17 -0300

    added cron job to fetch every 3 days

--- src/app/api/cron/ping-db/route.js
+++ src/app/api/cron/ping-db/route.js
@@ -1,4 +1,4 @@
-import { prisma } from "../../../lib/prisma.js";
+import { prisma } from "../../../../lib/prisma.js";
 import { NextResponse } from "next/server";
 
 export async function GET(request) {
